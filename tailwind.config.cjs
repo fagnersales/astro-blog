d80f21c5b25055371636593ae71e4a80b6ba50b7
Fagner Sales <fs.fagnersales@gmail.com>
2023-05-17 23:09:42 -0300

    change styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,9 @@ module.exports = {
 		extend: {
       gridTemplateColumns: {
         'auto-fit': 'repeat(auto-fit, minmax(350px, 1fr));'
+      },
+      animation: {
+        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite;'
       }
     },
 	},
